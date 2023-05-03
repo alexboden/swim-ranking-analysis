51667b0d6cd4b46efe1b25644e08115ff6fcfbba
alexboden <arboden@uwaterloo.ca>
2023-05-02 23:58:40 -0400

    added collapse/expand entries

--- static/collapsible-team.js
+++ static/collapsible-team.js
@@ -18,13 +18,38 @@ for (let i = 0; i < team_toggle_collapse_buttons.length; i++) {
     team_toggle_collapse_buttons[i].addEventListener('click', function() {
         let teamId = team_toggle_collapse_buttons[i].getAttribute('team-id');
 		teamId = teamId.split(' ')[0];
-		console.log(teamId)
         let div = document.getElementById('team-div-' + teamId);
-		console.log(div)
 		div.style.display = 'none';
     });
 }
 
+let team_collapse_entries_buttons = document.querySelectorAll('#btn-collapse-entries-team');
+for (let i = 0; i < team_collapse_entries_buttons.length; i++) {
+	team_collapse_entries_buttons[i].addEventListener('click', function() {
+		let teamId = team_collapse_entries_buttons[i].getAttribute('team-id');
+		teamId = teamId.split(' ')[0];
+		swimmers_tables = document.querySelectorAll("[table_team_id]");
+		for (let j = 0; j < swimmers_tables.length; j++) {
+			if (swimmers_tables[j].getAttribute('table_team_id') === teamId) {
+				swimmers_tables[j].style.display = 'none';
+			}
+		}
+	});
+}
+
+let team_expand_entries_buttons = document.querySelectorAll('#btn-expand-entries-team');
+for (let i = 0; i < team_expand_entries_buttons.length; i++) {
+	team_expand_entries_buttons[i].addEventListener('click', function() {
+		let teamId = team_expand_entries_buttons[i].getAttribute('team-id');
+		teamId = teamId.split(' ')[0];
+		swimmers_tables = document.querySelectorAll("[table_team_id]");
+		for (let j = 0; j < swimmers_tables.length; j++) {
+			if (swimmers_tables[j].getAttribute('table_team_id') === teamId) {
+				swimmers_tables[j].style.display = 'table';
+			}
+		}
+	});
+}
 
 
 let team_toggle_expand_buttons = document.querySelectorAll('#btn-expand-team');
